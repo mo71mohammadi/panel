392e0e0867590973715d282bce92396f0b6865d9
mo71.mohammadi <mo71.mohammadi@gmail.com>
2020-02-13 01:33:57 +0330

    fix interction

--- src/components/InteractionTable/columns.tsx
+++ src/components/InteractionTable/columns.tsx
@@ -1,64 +1,59 @@
-import React, { useState } from "react";
-import { Tag } from "antd";
+import React, {useState} from "react";
+import {Tag} from "antd";
 import Search from "antd/lib/input/Search";
 import axios from "axios";
 
 export function Columns(params: any) {
-  //const [state, setstate] = useState({ name: "", id: "" });
 
-  //const data = [{ alef: "aaaa", beh: "bbb" }];
-  console.log("params in Columns", params);
+    const columns = [
+        {
+            title: "enName",
+            dataIndex: "enName",
+            key: "enName"
+            //...getColumnSearchProps("IRC")
+        },
+        {
+            title: "enRoute",
+            dataIndex: "enRoute",
+            key: "enRoute",
+            render: function tag(params: any) {
+                return <Tag color={"red"}>{params}</Tag>;
+            }
+            // ...getColumnSearchProps("GTIN")
+        },
+        {
+            title: "upToDateId",
+            dataIndex: "upToDateId",
+            key: "upToDateId",
+            editable: true,
+            render: function (index: number, record: any) {
+                let Name: any = {name: '', id: ''};
+                if (index) Name = params.find((item: any) => item.id === index);
 
-  const columns = [
-    {
-      title: "enName",
-      dataIndex: "enName",
-      key: "enName"
-      //...getColumnSearchProps("IRC")
-    },
-    {
-      title: "enRoute",
-      dataIndex: "enRoute",
-      key: "enRoute",
-      render: function tag(params: any) {
-        return <Tag color={"red"}>{params}</Tag>;
-      }
-      // ...getColumnSearchProps("GTIN")
-    },
-    {
-      title: "upToDateId",
-      dataIndex: "upToDateId",
-      key: "upToDateId",
-      editable: true,
-      render: function(index: number, record: any) {
-        const Name: any[] = params.find(
-          (id: any) => id.id === record.upToDateId
-        );
-
-        if (Name === undefined) {
-          console.log("undefined");
+                const search = () => {
+                    return (
+                        <Search
+                            placeholder={Name.name}
+                            // onClick={handleClick}
+                            onSearch={value => console.log(value)}
+                            style={{width: 200}}
+                        />
+                    )
+                };
+                return search()
+            }
+            // ...getColumnSearchProps("PackageCount")
+        },
+        {
+            title: "medScapeId",
+            dataIndex: "medScapeId",
+            key: "medScapeId"
+            // ...getColumnSearchProps("cPrice")
         }
-        console.log("Name", Name);
-        console.log("record", record);
-
-        return (
-          <Search
-            //placeholder={Name.name}
-            onSearch={value => console.log(value)}
-            style={{ width: 200 }}
-          />
-        );
-      }
-
-      // ...getColumnSearchProps("PackageCount")
-    },
-    {
-      title: "medScapeId",
-      dataIndex: "medScapeId",
-      key: "medScapeId"
-      // ...getColumnSearchProps("cPrice")
-    }
-  ];
-
-  return columns;
+    ];
+    // const handleClick = () => {
+    //   setValue({...Name});
+    //
+    // };
+    return columns;
 }
--- src/components/InteractionTable/interactionTable.tsx
+++ src/components/InteractionTable/interactionTable.tsx
@@ -1,85 +1,77 @@
-import React, { useEffect, useState } from "react";
-import { Table, Alert } from "antd";
+import React, {useEffect, useState} from "react";
+import {Table, Alert} from "antd";
 import axios from "axios";
-import { Columns } from "./columns";
+import {Columns} from "./columns";
 
 export default function InteractionTable() {
-  const [tableData, setTableData] = useState([
-    {
-      enName: "",
-      enRoute: "",
-      upToDateId: "",
-      medScapeId: "",
-      Interaction: { name: "", id: "" }
-    }
-  ]);
-  const [state, setState] = useState([{ name: "", id: "" }]);
+    const [tableData, setTableData] = useState([]);
+    const [state, setState] = useState([]);
 
-  const [pagi, setPagi] = useState({
-    pageSize: 10,
-    pageCurrent: 1
-  });
-  const [count, setCount] = useState({ total: 0 });
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setLoading(true);
-    axios({
-      method: "post",
-      url: "http://45.92.95.69:5000/api/drugs/interaction",
-      data: { size: pagi.pageSize, page: pagi.pageCurrent }
-    })
-      .then((res: { data: any }) => {
-        setTableData(res.data.data);
-        setCount({ total: res.data.count });
-        setLoading(false);
-
-        axios({
-          method: "GET",
-          url: "http://45.92.95.69:5000/api/upToDate/name",
-          data: {}
-        })
-          .then((resp: any) => {
-            setState(resp.data);
-          })
-          .catch(() => console.log("Get upToDate Data Fail"));
-      })
-      .catch(() => console.log("Get Data Fail"));
-  }, [pagi.pageCurrent]);
-
-  const handleTableChange = (pagination: any, filters: any, sorter: any) => {
-    setPagi({
-      pageSize: pagination.pageSize,
-      pageCurrent: pagination.current
+    const [pagi, setPagi] = useState({
+        pageSize: 10,
+        pageCurrent: 1
     });
-  };
+    const [count, setCount] = useState({total: 0});
+    const [loading, setLoading] = useState(true);
 
-  return (
-    <div style={{ background: "#fafafa" }}>
-      <div>
-        <Table
-          loading={loading}
-          rowKey={(record: any) => record.tableData}
-          size="small"
-          columns={Columns(state)}
-          dataSource={tableData}
-          pagination={{
-            total: count.total,
-            position: "bottom",
-            defaultCurrent: 1,
-            current: pagi.pageCurrent
-          }}
-          scroll={{ x: 600 }}
-          onChange={handleTableChange}
-        />
-      </div>
-      <div style={{ width: "100%", display: "flex" }}>
-        <Alert
-          style={{ width: "50%" }}
-          message={`Total item in database is ${count.total} `}
-          type="success"
-        />
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url: "http://45.92.95.69:5000/api/upToDate/name",
+            data: {}
+        })
+            .then((res: any) => {
+                setState(res.data);
+            })
+            .catch(() => console.log("Get upToDate Data Fail"));
+
+        setLoading(true);
+        axios({
+            method: "post",
+            url: "http://45.92.95.69:5000/api/drugs/interaction",
+            data: {size: pagi.pageSize, page: pagi.pageCurrent}
+        })
+            .then((res: { data: any }) => {
+                setTableData(res.data.data);
+                setCount({total: res.data.count});
+                setLoading(false);
+            })
+            .catch(() => console.log("Get Data Fail"));
+    }, [pagi.pageCurrent]);
+
+    const handleTableChange = (pagination: any, filters: any, sorter: any) => {
+        setPagi({
+            pageSize: pagination.pageSize,
+            pageCurrent: pagination.current
+        });
+    };
+
+    return (
+        <div style={{background: "#fafafa"}}>
+            <div>
+                <Table
+                    loading={loading}
+                    rowKey={(record: any) => record.tableData}
+                    size="small"
+                    columns={Columns(state)}
+                    dataSource={tableData}
+                    pagination={{
+                        total: count.total,
+                        position: "bottom",
+                        defaultCurrent: 1,
+                        current: pagi.pageCurrent
+                    }}
+                    scroll={{x: 600}}
+                    onChange={handleTableChange}
+                />
+            </div>
+            <div style={{width: "100%", display: "flex"}}>
+                <Alert
+                    style={{width: "50%"}}
+                    message={`Total item in database is ${count.total} `}
+                    type="success"
+                />
+            </div>
+        </div>
+    );
 }

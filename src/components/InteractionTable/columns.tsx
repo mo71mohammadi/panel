129aa122df735acaae7f0d994f069af8064da8b2
baziar <mail@baziar.dev>
2020-02-13 03:00:39 -0500

    optimize

--- src/components/InteractionTable/columns.tsx
+++ src/components/InteractionTable/columns.tsx
@@ -1,59 +1,54 @@
-import React, {useState} from "react";
-import {Tag} from "antd";
+import React, { useState } from "react";
+import { Tag } from "antd";
 import Search from "antd/lib/input/Search";
-import axios from "axios";
 
 export function Columns(params: any) {
+  const columns = [
+    {
+      title: "enName",
+      dataIndex: "enName",
+      key: "enName"
+      //...getColumnSearchProps("IRC")
+    },
+    {
+      title: "enRoute",
+      dataIndex: "enRoute",
+      key: "enRoute",
+      render: function tag(params: any) {
+        return <Tag color={"red"}>{params}</Tag>;
+      }
+      // ...getColumnSearchProps("GTIN")
+    },
+    {
+      title: "upToDateId",
+      dataIndex: "upToDateId",
+      key: "upToDateId",
+      editable: true,
+      render: function(index: number, record: any) {
+        let Name: any = { name: "", id: "" };
+        if (index) Name = params.find((item: any) => item.id === index);
 
-    const columns = [
-        {
-            title: "enName",
-            dataIndex: "enName",
-            key: "enName"
-            //...getColumnSearchProps("IRC")
-        },
-        {
-            title: "enRoute",
-            dataIndex: "enRoute",
-            key: "enRoute",
-            render: function tag(params: any) {
-                return <Tag color={"red"}>{params}</Tag>;
-            }
-            // ...getColumnSearchProps("GTIN")
-        },
-        {
-            title: "upToDateId",
-            dataIndex: "upToDateId",
-            key: "upToDateId",
-            editable: true,
-            render: function (index: number, record: any) {
-                let Name: any = {name: '', id: ''};
-                if (index) Name = params.find((item: any) => item.id === index);
-
-                const search = () => {
-                    return (
-                        <Search
-                            placeholder={Name.name}
-                            // onClick={handleClick}
-                            onSearch={value => console.log(value)}
-                            style={{width: 200}}
-                        />
-                    )
-                };
-                return search()
-            }
-            // ...getColumnSearchProps("PackageCount")
-        },
-        {
-            title: "medScapeId",
-            dataIndex: "medScapeId",
-            key: "medScapeId"
-            // ...getColumnSearchProps("cPrice")
-        }
-    ];
-    // const handleClick = () => {
-    //   setValue({...Name});
-    //
-    // };
-    return columns;
+        return (
+          <Search
+            placeholder={Name.name}
+            //onClick={handleClick}
+            onSearch={value => console.log(value)}
+            style={{ width: 200 }}
+          />
+        );
+      }
+      // ...getColumnSearchProps("PackageCount")
+    },
+    {
+      title: "medScapeId",
+      dataIndex: "medScapeId",
+      key: "medScapeId"
+      // ...getColumnSearchProps("cPrice")
+    }
+  ];
+  // const handleClick = () => {
+  //   setValue({...Name});
+  //
+  // };
+  return columns;
 }

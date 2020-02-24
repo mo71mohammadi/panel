19db94dcd6b6cf0157b5e86fb6a9c7902f1f550c
mo71.mohammadi <mo71.mohammadi@gmail.com>
2020-02-24 20:45:04 +0330

    fix logout

--- src/pageManager/Route.js
+++ src/pageManager/Route.js
@@ -1,6 +1,5 @@
 import React, { useState, useContext } from "react";
 import { Redirect, Route } from 'react-router-dom'
-// import propTimport { LoginState } from "./loginState";
 import { LoginState } from "../components/login/loginState";
 
 export default function RouteWrapper({
@@ -10,14 +9,6 @@ export default function RouteWrapper({
     const signed = false;
     const { login, setLogin } = useContext(LoginState);
 
-
-    // if (isPrivate && !signed) {
-    //     return <Redirect to='/Interaction' />;
-    // }
-    // if (!isPrivate && signed) {
-    //     return <Redirect to='/Interaction' />;
-    // }
-
     return <Route {...rest} render={props => {
         console.log(props)
         if (!login.isAuthenticated) return <Redirect to='/Login'/>
@@ -25,12 +16,3 @@ export default function RouteWrapper({
         else return <Component {...props} />
     }} />;
 }
-
-// RouteWrapper.propTypes = {
-//     isPrivate: propTypes.bool,
-//     component: propTypes.oneOfType([propTypes.element, propTypes.func]).isRequired
-// }
-
-// RouteWrapper.defaultProps = {
-//     isPrivate: true,
-// }
